14ae6dad2eaeb43af9b29f12627532b328b53439
Dewidos <hopor37@gmail.com>
2021-12-12 17:19:20 +0100

    Added two slash commands

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 import DiscordJS, { Intents, Message } from 'discord.js'
 import 'dotenv/config'
 
+const playersToStalk: Array<string> = []
+
 const client = new DiscordJS.Client({
 	intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS],
 })
@@ -9,15 +11,30 @@ client.on('ready', () => {
 	console.log('Hello world!')
 })
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return
+
+	const { commandName } = interaction
+
+	switch (commandName) {
+		case 'ping':
+			await interaction.reply('pong')
+			break
+		case 'stalking':
+			if (playersToStalk.indexOf(interaction.user.id) == -1) {
+				playersToStalk.push(interaction.user.id)
+				await interaction.reply('Od teraz będę Cię stalkował :)')
+			} else {
+				playersToStalk.splice(playersToStalk.indexOf(interaction.user.id), 1)
+				await interaction.reply('Od teraz nie będę Ciebie stalkować :(')
+			}
+			break
+	}
+})
+
 client.on('messageCreate', msg => {
-	if (msg.content === 'empty message pls') {
-		msg.channel.sendTyping().then(() => {
-			setTimeout(() => {
-				msg.channel.send('n i e')
-			}, 3000)
-		})
-	} else if (msg.author.tag === 'Dewidos#8177') {
-		msg.reply('Siema Dewidos!')
+	if (playersToStalk.indexOf(msg.author.id) != -1) {
+		msg.reply(`Siema ${msg.author.username}!`)
 	}
 })
 
