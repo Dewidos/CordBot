898f6e097e217bab784730a1b5bbef32c32aeba4
Dewidos <hopor37@gmail.com>
2021-12-15 17:39:35 +0100

    Added counting channel feature

--- main.ts
+++ main.ts
@@ -2,13 +2,35 @@ import DiscordJS, { Intents, MessageAttachment } from 'discord.js'
 import 'dotenv/config'
 
 const playersToStalk: Array<string> = []
+let actualNumber = 0
 
 const client = new DiscordJS.Client({
 	intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS],
 })
 
-client.on('ready', () => {
+client.on('ready', async () => {
 	console.log('Hello world!')
+
+	if (typeof process.env.COUNTING_CHANNEL === 'undefined') return
+
+	const countingChannel = client.channels.cache.get(process.env.COUNTING_CHANNEL!) as DiscordJS.TextChannel
+
+	countingChannel.messages.fetch().then(messages => {
+		let lastNumber: number
+
+		for (let i = messages.size - 1; i >= 0; i--) {
+			let msg = messages.at(i)!
+
+			lastNumber = parseInt(msg.content!)
+
+			if (isNaN(lastNumber)) {
+				if (msg.deletable) msg.delete()
+				continue
+			}
+
+			if (lastNumber >= 0) actualNumber = lastNumber
+		}
+	})
 })
 
 client.on('interactionCreate', async interaction => {
@@ -38,12 +60,31 @@ client.on('messageCreate', async msg => {
 	if (msg.channel.id === process.env.REPEATER_CHANNEL) {
 		const attachmentsArray: Array<MessageAttachment> = []
 
-		if (msg.attachments.size > 0) msg.attachments.forEach(att => attachmentsArray.push(att))
+		if (msg.content.length > 30) {
+			const botReply = await msg.reply(
+				`Maksymalna długość wiadomości jaką mam powtórzyć to 30 znaków. Twoja wiadomość liczy aż ${msg.content.length}!`
+			)
 
-		await msg.channel.send({
-			content: msg.content == '' ? '\n' : msg.content,
-			files: attachmentsArray,
-		})
+			if (msg.deletable) await msg.delete()
+
+			setTimeout(() => botReply.delete(), 5000)
+		} else {
+			if (msg.attachments.size > 0) msg.attachments.forEach(att => attachmentsArray.push(att))
+
+			await msg.channel.send({
+				content: msg.content == '' ? '\n' : msg.content,
+				files: attachmentsArray,
+			})
+		}
+	} else if (msg.channel.id === process.env.COUNTING_CHANNEL) {
+		let parsedNumber = parseInt(msg.content)
+
+		if (isNaN(parsedNumber)) {
+			if (msg.deletable) msg.delete()
+		} else {
+			if (parsedNumber - 1 === actualNumber) actualNumber++
+			else if (msg.deletable) msg.delete()
+		}
 	} else if (playersToStalk.indexOf(msg.author.id) != -1) {
 		await msg.reply(`Siema ${msg.author.username}!`)
 	}
