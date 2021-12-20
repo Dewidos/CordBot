c35f6ac5a2ccc8cfbd7cb753229293834c19e4c9
Dewidos <hopor37@gmail.com>
2021-12-20 17:54:11 +0000

    Updated npm packages

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
+cd /home/pi/CordBot
 echo "Pulling the latest changes from the repository..."
 git pull
 echo "Compiling the bot's code..."
 tsc --skipLibCheck
 echo "Starting the bot..."
 node main.js
+exit 0
